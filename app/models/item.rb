12f44c33e562b8e1473aab1b1f3f5fd09ae5c6b8
Charlie Egan <charlieegan3@googlemail.com>
2015-03-07 12:34:14 +0000

    truncate_title before save

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
   validates_uniqueness_of :url
+
+  before_save :truncate_title
+  def truncate_title
+    self.title = self.title[0..140] + '...' if self.title.length > 140
+  end
 end

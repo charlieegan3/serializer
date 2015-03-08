9bbd8e46f3da656f73d24cfd4d446d42645b9218
Charlie Egan <charlieegan3@googlemail.com>
2015-03-08 10:27:09 +0000

    Add location label below logo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
-  get 'all' => 'application#all'
+  get 'all' => 'application#all', as: :all
   get 'custom' => 'application#custom', as: :custom
   get 'add_source' => 'application#add_source', as: :add_source
 

e8ab5a7e4848a388d59325faf1df3f42fb6847a9
Charlie Egan <charlieegan3@googlemail.com>
2015-03-14 13:18:14 +0000

    Create CloudinaryImage model
    Saves current graph

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312220729) do
+ActiveRecord::Schema.define(version: 20150314125448) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cloudinary_images", force: :cascade do |t|
+    t.string   "identifier"
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "url"

42044369fbdd2c6a799c844181cb8d635062fbc4
Charlie Egan <charlieegan3@googlemail.com>
2015-03-19 10:37:32 +0000

    Truncate/compact muted link detail content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def time_ago(time)
+    (distance_of_time_in_words(time, Time.new) + ' ago').
+      gsub('about', '').
+      gsub('hours', 'hrs').
+      gsub('minutes', 'mins').
+      gsub('less than a', '1').
+      strip
+  end
+
+  def print_url(url)
+    url.split('/')[2].gsub('www.', '')
+  end
 end

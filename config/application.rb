776ec7f4e7bb31171a65594fc894b2ebdbe93594
Charlie Egan <charlieegan3@googlemail.com>
2015-03-22 16:28:57 +0000

    Use Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Serializer
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += %W(#{config.root}/lib)
+
+    config.middleware.use Rack::Deflater
   end
 end

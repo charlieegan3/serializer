dd85f0c3dd748e1349c061dddf465dace7b761f1
Charlie Egan <charlieegan3@googlemail.com>
2015-03-24 16:39:46 +0000

    Add Item specs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ActiveRecord::Base
+  validates_presence_of :title
   validates_uniqueness_of :url
+  validates_presence_of :url
 
   before_save :truncate_title
   def truncate_title
-    self.title = self.title[0..140] + '...' if self.title.length > 140
+    self.title = self.title[0..140] + '...' if self.title && self.title.length > 140
   end
 
   def self.matching(sources = ['hacker_news', 'reddit', 'product_hunt'])
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_uniqueness_of(:url) }
+  it { should validate_presence_of(:url) }
+  it { should validate_presence_of(:title) }
+
+  it 'should truncate titles longer than 140chrs' do
+    item = create(:item, title: 'news ' * 100)
+    expect(item.title.length).to eq(144)
+  end
+
+  it 'returns matching items' do
+    for i in 0...SOURCES.size
+      create(:item, source: SOURCES[i])
+    end
+    expect(Item.matching.size).to eq(3)
+    expect(Item.matching(SOURCES).size).to eq(SOURCES.size)
+  end
+
+  it 'returns the days hour count' do
+    24.times do
+      create(:item, created_at: Time.zone.now - 1.days)
+      create(:item, created_at: Time.zone.now)
+    end
+    expect(Item.average_hour_count(SOURCES)).to eq(1)
+  end
 end

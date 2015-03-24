d08b51dc41ba2c51281d67410b78d838ad3a451a
Charlie Egan <charlieegan3@googlemail.com>
2015-03-24 16:39:46 +0000

    Add Item specs

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,14 @@
 FactoryGirl.define do
+  sequence(:url) { |n| "http://www.serializer.io/#{n}" }
   factory :item do
-    
+    title 'serializer.io'
+    url
+    source 'hacker_news'
+    topped true
+    comment_url 'http://www.serializer.io/comments'
+    redirect_url 'http://www.serializer.io/welcome'
+    tweet_count 100
+    created_at Time.zone.now
+    updated_at Time.zone.now
   end
-
 end

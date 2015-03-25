b5b6a04e977e7a7d696877603e5efb3c3130d9ee
Charlie Egan <charlieegan3@googlemail.com>
2015-03-25 20:49:03 +0000

    Add application helper specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,42 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'time_ago' do
+    it 'removes "about"' do
+      expect(helper.time_ago((1.5).hours.ago)).to_not include('about')
+    end
+
+    it 'removes "less than"' do
+      expect(helper.time_ago(10.seconds.ago)).to_not include('less than')
+    end
+
+    it 'shortens to "hrs"' do
+      expect(helper.time_ago(3.hours.ago)).to include('hrs')
+    end
+
+    it 'shortens to "mins"' do
+      expect(helper.time_ago(3.minutes.ago)).to include('mins')
+    end
+
+    it 'appends "ago"' do
+      expect(helper.time_ago(rand(1..10).hours.ago)).to include('ago')
+    end
+  end
+
+  describe 'print_url' do
+    it 'removes params' do
+      url = 'http://www.google.com?some=junk-on-the-end'
+      expect(helper.print_url(url)).to eq('google.com')
+    end
+
+    it 'removes www' do
+      url = 'http://www.google.com'
+      expect(helper.print_url(url)).to eq('google.com')
+    end
+
+    it 'cuts truncates to the limit' do
+      url = 'http://www.google.com'
+      expect(helper.print_url(url, 5)).to eq('google...')
+    end
+  end
 end

cee472ea7d3b9a792853156cb0a52f79c938ed6e
Charlie Egan <charlieegan3@googlemail.com>
2015-04-01 17:27:46 +0100

    Include word_count in item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,7 @@ FactoryGirl.define do
     comment_url 'http://www.serializer.io/comments'
     redirect_url 'http://www.serializer.io/welcome'
     tweet_count 100
+    word_count 500
     created_at Time.zone.now
     updated_at Time.zone.now
   end

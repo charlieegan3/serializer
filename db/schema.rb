3c9ce22f7af48482505b323cda003a12817e097a
Charlie Egan <charlieegan3@googlemail.com>
2015-04-01 17:14:48 +0100

    Implement word count feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318223350) do
+ActiveRecord::Schema.define(version: 20150331204530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150318223350) do
     t.string   "comment_url"
     t.string   "redirect_url"
     t.integer  "tweet_count"
+    t.integer  "word_count"
   end
 
   create_table "sessions", force: :cascade do |t|

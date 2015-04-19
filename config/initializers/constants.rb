30d12bf2b0f58e4046051b6d83bc24188d659276
Charlie Egan <charlieegan3@googlemail.com>
2015-04-19 15:35:10 +0100

    Remove keys and secrets

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,5 @@
 SOURCES = ['hacker_news', 'product_hunt', 'reddit', 'lobsters', 'slashdot', 'beta_list', 'macrumors', 'qudos', 'designer_news', 'arstechnica']
-TRELLO_KEY = 'c039d395cd584a66a4da41e12028719e'
+TRELLO_KEY = ''
+GMAIL_ACCOUNT = ''
+GMAIL_PASSWORD = ''
+NOTIFY_EMAIL = ''
--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,8 +1,8 @@
 module Notifier
   def send_errors(errors)
-    Gmail.connect('charlie.notifications', 'notifications') do |gmail|
+    Gmail.connect(GMAIL_ACCOUNT, GMAIL_PASSWORD) do |gmail|
       gmail.deliver do
-        to 'accounts@charlieegan3.com'
+        to NOTIFY_EMAIL
         subject 'Error Notifier'
         text_part do
           body errors.join("\n\n")
@@ -12,9 +12,9 @@ module Notifier
   end
 
   def send_feedback(comment, user_agent)
-    Gmail.connect('charlie.notifications', 'notifications') do |gmail|
+    Gmail.connect(GMAIL_ACCOUNT, GMAIL_PASSWORD) do |gmail|
       gmail.deliver do
-        to 'accounts@charlieegan3.com'
+        to NOTIFY_EMAIL
         subject 'Feedback Notifier'
         text_part do
           body "#{comment}\n\n#{user_agent}"

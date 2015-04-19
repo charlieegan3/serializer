dca5cbb5042a1713bbf94f2643157078afa90c65
Charlie Egan <charlieegan3@googlemail.com>
2015-04-19 12:04:52 +0100

    Update scheduled tasks

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -6,6 +6,18 @@ scheduler.every '10m' do
   `rake collect_active`
 end
 
-scheduler.every '60m' do
+scheduler.every '10m' do
+  `rake set_tweet_counts`
+end
+
+scheduler.every '1h' do
   `rake collect_feeds`
+end
+
+scheduler.every '1h' do
+  # `rake save_graph`
+end
+
+scheduler.every '1d' do
+  `rake clean_inactive_sessions`
 end

8d37e5b8e8afe2c5133ca5be9739e70f645c64eb
Charlie Egan <charlieegan3@googlemail.com>
2015-04-19 21:26:23 +0100

    Add local_env

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ module Serializer
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += %W(#{config.root}/lib)

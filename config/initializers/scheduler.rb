79c5e9f183f86186f5f51d07e53330e8ca20770b
Charlie Egan <charlieegan3@googlemail.com>
2015-05-16 22:02:11 +0100

    Only scedule tasks in production environment

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,23 +1,25 @@
 require 'rufus-scheduler'
 
-scheduler = Rufus::Scheduler.singleton
+if Rails.env.production?
+  scheduler = Rufus::Scheduler.singleton
 
-scheduler.every '10m' do
-  `rake collect_active`
-end
+  scheduler.every '10m' do
+    `rake collect_active`
+  end
 
-scheduler.every '10m' do
-  `rake set_tweet_counts`
-end
+  scheduler.every '10m' do
+    `rake set_tweet_counts`
+  end
 
-scheduler.every '1h' do
-  `rake collect_feeds`
-end
+  scheduler.every '1h' do
+    `rake collect_feeds`
+  end
 
-scheduler.every '1h' do
-  `rake save_graph`
-end
+  scheduler.every '1h' do
+    `rake save_graph`
+  end
 
-scheduler.every '1d' do
-  `rake clean_inactive_sessions`
+  scheduler.every '1d' do
+    `rake clean_inactive_sessions`
+  end
 end

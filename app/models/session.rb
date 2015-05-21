e85f3d799a14899bfa32a684a7a5aa20a3973ece
Charlie Egan <charlieegan3@googlemail.com>
2015-05-21 10:49:03 +0100

    Reduce chances of session identifier collision

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,9 +2,7 @@ class Session < ActiveRecord::Base
   serialize :sources, Array
   serialize :saved_items, Array
   validates_uniqueness_of :identifier
-  before_save do
-    self.identifier = RandomUsername.username if self.identifier.blank?
-  end
+  validates_presence_of :identifier
 
   def update_sources(source)
     sources.include?(source)? sources.delete(source) : sources << source
@@ -12,6 +10,19 @@ class Session < ActiveRecord::Base
   end
 
   def self.find_or_create(identifier)
-    find_by_identifier(identifier) || create(identifier: identifier)
+    if identifier.blank?
+      create(identifier: self.generate_identifier)
+    else
+      find_by_identifier(identifier) || create(identifier: self.generate_identifier)
+    end
+  end
+
+  def self.generate_identifier
+    RandomUsername.username.tap do |identifier|
+      10.times do
+        break unless Session.find_by_identifier(identifier)
+        identifier = RandomUsername.username
+      end
+    end
   end
 end

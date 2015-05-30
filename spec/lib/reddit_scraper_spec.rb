69668f825ac7b83c49de8b8841e262b9f2b2f7a9
Charlie Egan <charlieegan3@googlemail.com>
2015-05-30 23:18:34 +0100

    Implement new reddit scraper

---
+++ spec/lib/reddit_scraper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe RedditScraper do
+  before(:all) do
+    @rs = RedditScraper.new
+  end
+
+  it 'should return items' do
+    allow_any_instance_of(RedditScraper).to receive(:word_count)
+      .and_return(0)
+    VCR.use_cassette('reddit') do
+      items = @rs.items
+      expect(items).to_not be_nil
+      keys = [:title, :url, :word_count, :comment_url, :source, :topped]
+      expect(items.sample.keys - keys).to be_empty
+    end
+  end
+
+  describe 'reject_item?' do
+    it 'should reject duplicate items' do
+      create(:item, url: 'http://www.serializer.io/')
+      new_item = { title: 'serializer', url: 'http://www.serializer.io/' }
+      expect(@rs.send(:reject_item?, new_item)).to be true
+    end
+
+    it 'should reject relative links' do
+      new_item = { url: '/thing' }
+      expect(@rs.send(:reject_item?, new_item)).to be true
+    end
+  end
+end

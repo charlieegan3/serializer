19899432086b376276d97507e2a45369200d37c0
Charlie Egan <charlieegan3@googlemail.com>
2015-05-31 10:05:40 +0100

    Implement new lobsters scraper

---
+++ lib/lobsters_scraper.rb
@@ -0,0 +1,53 @@
+class LobstersScraper
+  include Scraper
+  def initialize
+    @url = 'https://lobste.rs'
+  end
+
+  def items
+    [].tap do |items|
+      stories.each_with_index do |story, index|
+        next if reject_item?(item = { title: title(story), url: url(story) })
+        items << complete_item(item, story, index)
+      end
+    end
+  end
+
+  private
+
+  def stories
+    Nokogiri::HTML(open('https://lobste.rs'), nil, 'UTF-8')
+      .css('.details')
+  end
+
+  def title(story)
+    story.at_css('.link a').text
+  end
+
+  def url(story)
+    href = story.at_css('.link a')['href']
+    (href[0] == '/') ? href.prepend('https://lobste.rs') : href
+  end
+
+  def reject_item?(item)
+    item[:title].downcase.include?('working on this week') ||
+      Item.find_by_url(item[:url]).present?
+  end
+
+  def comment_url(story)
+    if (a = story.at_css('.comments_label a')).present?
+      a['href'].prepend('https://lobste.rs')
+    else
+      nil
+    end
+  end
+
+  def complete_item(item, story, index)
+    item.merge({
+      comment_url: comment_url(story),
+      source: 'lobsters',
+      topped: (index == 0) ? true : false,
+      word_count: word_count(item[:url]),
+    })
+  end
+end
---
+++ spec/lib/lobsters_scraper_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe LobstersScraper do
+  before(:all) do
+    @ls = LobstersScraper.new
+  end
+
+  it 'should return items' do
+    allow_any_instance_of(LobstersScraper).to receive(:word_count)
+      .and_return(0)
+    VCR.use_cassette('lobsters') do
+      items = @ls.items
+      expect(items).to_not be_nil
+      keys = [:title, :url, :word_count, :comment_url, :source, :topped]
+      expect(items.sample.keys - keys).to be_empty
+    end
+  end
+
+  it 'should reject items with an invalid title' do
+    expect(@ls.send(:reject_item?, { title: 'working on this week' })).to be true
+  end
+
+  it 'should reject items with an invalid title' do
+    expect(@ls.send(:reject_item?, { title: 'working on this week' })).to be true
+  end
+
+  it 'should reject duplicate items' do
+    create(:item, url: 'http://serializer.io/')
+    new_item = { title: 'serializer', url: 'http://serializer.io/' }
+    expect(@ls.send(:reject_item?, new_item)).to be true
+  end
+end

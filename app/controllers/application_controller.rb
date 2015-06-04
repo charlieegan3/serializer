c780a6d49218ca8d5e76f5772840f7d24d98e67b
Charlie Egan <charlieegan3@googlemail.com>
2015-06-04 22:15:02 +0100

    Remove unused Notifier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include Notifier
-
   def index
     return render json: Item.matching if params[:format] == 'json'
 

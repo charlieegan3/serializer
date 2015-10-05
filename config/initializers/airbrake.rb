6541160a2da6231fd0a37188bd7987197793b3ea
Charlie Egan <charlieegan3@googlemail.com>
2015-10-05 16:10:47 +0100

    Ignore additional exception types on airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,10 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_KEY']
   config.development_environments = []
+  config.ignore << 'Errno::ENETUNREACH'
+  config.ignore << 'SocketError'
   config.ignore << 'Timeout::Error'
   config.ignore << 'OpenURI::HTTPError'
   config.ignore << 'PG::UniqueViolation'
+  config.ignore << 'ActiveRecord::RecordInvalid'
 end

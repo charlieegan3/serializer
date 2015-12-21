cf9581d182cae033fe9bf6a9a2a0e8ac4de1b419
Charlie Egan <git@charlieegan3.com>
2015-12-21 23:04:15 +0000

    Remove old schema references to reading lists

--- db/schema.rb
+++ db/schema.rb
@@ -39,25 +39,6 @@ ActiveRecord::Schema.define(version: 20151130225344) do
 
   add_index "items", ["url"], name: "index_items_on_url", unique: true, using: :btree
 
-  create_table "reading_list_items", force: :cascade do |t|
-    t.integer  "reading_list_id"
-    t.integer  "item_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "reading_list_items", ["item_id"], name: "index_reading_list_items_on_item_id", using: :btree
-  add_index "reading_list_items", ["reading_list_id"], name: "index_reading_list_items_on_reading_list_id", using: :btree
-
-  create_table "reading_lists", force: :cascade do |t|
-    t.integer  "session_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "position"
-  end
-
-  add_index "reading_lists", ["session_id"], name: "index_reading_lists_on_session_id", using: :btree
-
   create_table "sessions", force: :cascade do |t|
     t.string   "identifier"
     t.string   "sources"
@@ -70,7 +51,4 @@ ActiveRecord::Schema.define(version: 20151130225344) do
     t.integer  "read_count"
   end
 
-  add_foreign_key "reading_list_items", "items"
-  add_foreign_key "reading_list_items", "reading_lists"
-  add_foreign_key "reading_lists", "sessions"
 end

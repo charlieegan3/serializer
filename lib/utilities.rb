a5eced0472325fe07a7e76b0492126505008df0c
Charlie Egan <git@charlieegan3.com>
2016-02-18 16:56:23 +0000

    Add timeout to final url collection
    Motivation:
    -task was taking too long on product hunt collection
    
    Change Explanation:
    -fail after 2 seconds using timeout and rescue as before

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -3,9 +3,11 @@ require 'timeout'
 module Utilities
   def final_url(redirect_url)
     begin
-      open(redirect_url, allow_redirections: :all) do |resp|
-        return resp.base_uri.to_s
-      end
+      return Timeout::timeout(2) {
+        open(redirect_url, allow_redirections: :all) do |resp|
+          return resp.base_uri.to_s
+        end
+      }
     rescue
       return HTTPClient.new.get(redirect_url).header['Location'].first.to_s
     end

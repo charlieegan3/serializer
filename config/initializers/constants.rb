ebc2c52d2023c59fc8ce6a462171e1695e44ea09
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-01-27 21:56:27 +0000

    Remove other sources

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,13 +1,1 @@
-SOURCES = [
-  'hacker_news',
-  'product_hunt',
-  'reddit',
-  'lobsters',
-  'slashdot',
-  'beta_list',
-  'macrumors',
-  'designer_news',
-  'arstechnica',
-  'computerphile',
-  'techcrunch'
-]
+SOURCES = ['hacker_news']

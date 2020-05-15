ef7c0dfa76d0bbc602ea0e55b8d3fe2e936d97f5
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-05-15 20:23:29 +0100

    Update menu toggle and overlay style

--- app/assets/javascripts/features/toggle_settings_panel.js
+++ app/assets/javascripts/features/toggle_settings_panel.js
@@ -5,8 +5,10 @@ DOMReady(function () {
     var settingsPanel = document.getElementById('settings-panel');
     if (settingsPanel.className === 'hidden') {
       settingsPanel.setAttribute('class', '');
+      toggleButton.innerHTML = "close";
     } else {
       settingsPanel.setAttribute('class', 'hidden');
+      toggleButton.innerHTML = "menu";
     }
   }
 });
